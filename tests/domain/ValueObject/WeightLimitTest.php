ba7f7902264b85e4ffbf829b051e204aec8898be
André Silva <andresilva@aiqfome.com>
2020-08-20 05:39:37 -0300

    Improve test variable names

--- tests/domain/ValueObject/WeightLimitTest.php
+++ tests/domain/ValueObject/WeightLimitTest.php
@@ -3,38 +3,38 @@
 use Domain\ValueObject\WeightLimit;
 
 $empty = new WeightLimit();
-$small = new WeightLimit(null, 5);
-$large = new WeightLimit(5.1);
+$light = new WeightLimit(null, 5);
+$normal = new WeightLimit(5.1);
 
-it('asserts weight limit instance', function() use ($empty, $small, $large) {
+it('asserts weight limit instance', function() use ($empty, $light, $normal) {
     assertInstanceOf(WeightLimit::class, $empty);
-    assertInstanceOf(WeightLimit::class, $small);
-    assertInstanceOf(WeightLimit::class, $large);
+    assertInstanceOf(WeightLimit::class, $light);
+    assertInstanceOf(WeightLimit::class, $normal);
 });
 
-it('asserts weight limit values', function() use ($empty, $small, $large) {
+it('asserts weight limit values', function() use ($empty, $light, $normal) {
     assertNull($empty->minWeight());
     assertNull($empty->maxWeight());
 
-    assertNull($small->minWeight());
-    assertEquals(5, $small->maxWeight());
+    assertNull($light->minWeight());
+    assertEquals(5, $light->maxWeight());
 
-    assertNull($large->maxWeight());
-    assertEquals(5.1, $large->minWeight());
+    assertNull($normal->maxWeight());
+    assertEquals(5.1, $normal->minWeight());
 });
 
 it('asserts can handle when weight limit is empty')
     ->assertTrue($empty->canHandle(100));
 
-it('asserts can handle weight limit small limits', function() use ($small) {
-    assertTrue($small->canHandle(0));
-    assertTrue($small->canHandle(5));
-    assertFalse($small->canHandle(5.1));
+it('asserts can handle weight limit light limits', function() use ($light) {
+    assertTrue($light->canHandle(0));
+    assertTrue($light->canHandle(5));
+    assertFalse($light->canHandle(5.1));
 });
 
-it('asserts can handle weight limit large limits', function() use ($large) {
-    assertFalse($large->canHandle(0));
-    assertFalse($large->canHandle(5));
-    assertTrue($large->canHandle(5.1));
-    assertTrue($large->canHandle(200));
+it('asserts can handle weight limit normal limits', function() use ($normal) {
+    assertFalse($normal->canHandle(0));
+    assertFalse($normal->canHandle(5));
+    assertTrue($normal->canHandle(5.1));
+    assertTrue($normal->canHandle(200));
 });
