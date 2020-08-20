e1b03f2e53a97eb9fa8f2916c482c906f077b6e0
André Silva <andresilva@aiqfome.com>
2020-08-20 06:30:02 -0300

    Add products into homepage

--- src/Controller/Web/HomeController.php
+++ src/Controller/Web/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Web;
 
+use App\Service\ShipmentCostService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,10 @@ class HomeController extends AbstractController
    /**
     * @Route("/")
     */
-    public function index(): Response
+    public function index(ShipmentCostService $shipmentCostService): Response
     {
-        return $this->render('home/index.html.twig');
+        $products = $shipmentCostService->orderProductsShippingCostBySupplier();
+
+        return $this->render('home/index.html.twig', compact('products'));
     }
 }
