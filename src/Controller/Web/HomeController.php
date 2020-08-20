4289d544e7b56464e3c2beb8ec886b8404fca80c
André Silva <andresilva@aiqfome.com>
2020-08-20 03:52:54 -0300

    Fix home template and remove Random Number references

--- src/Controller/Web/HomeController.php
+++ src/Controller/Web/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Web;
 
-use Domain\ValueObject\RandomNumber;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,10 +13,6 @@ class HomeController extends AbstractController
     */
     public function index(): Response
     {
-        $number = new RandomNumber();
-
-        return $this->render('home/index.html.twig', [
-          'number' => $number->value(),
-      ]);
+        return $this->render('home/index.html.twig');
     }
 }
